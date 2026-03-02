4618054a1698d83007057f05810cb83295fcde79
NGOMEFERNANDEZ <ngomefernandezm@gmail.com>
2026-03-02 18:51:46 +0100

    Added Python file with basic syntax

---
+++ note.py
@@ -0,0 +1,10 @@
+name = "Fefe"
+age = 20
+field = "Computer Engineering"
+
+print("Name:", name)
+print("Age:", age)
+print("Field:", field)
+
+for i in range(3):
+    print("Building strong foundations in Git and Python")
